5ddceda2d481e18152e36575bfe341971dba1a9a
Enyziee <kenedysamuel63@gmail.com>
2025-08-31 23:29:09 -0300

    setup bun template

--- src/app.ts
+++
@@ -1,7 +0,0 @@
-import { ExtendedClient } from "./types/ExtendedClient.js";
-import 'dotenv/config';
-
-const client = new ExtendedClient();
-client.start();
-
-export { client };
--- src/utils/test.ts
+++
@@ -1,5 +0,0 @@
-const url1 = "https://www.youtube.com/watch?v=xZ4IIKRnP1k";
-const url2 = "https://youtu.be/xZ4IIKRnP1k";
-const url3 = "https://music.youtube.com/watch?v=G3dvGEmqUSg&si=rzLiZdIsgJ_-_FK0";
-
-console.log(url1.startsWith("https://www.youtube.com/"));
